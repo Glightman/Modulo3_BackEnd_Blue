6a34f3e9d8562c51b51ef0733a111650992ad53d
Gabriel Santana <gabriellima36716@gmail.com>
2021-07-24 15:47:22 -0300

    new project: RPG game; 4 class

---
+++ C#_projects/RPG_aula4/Play.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_aula4
+{
+    public class Play
+    {
+        public void Start()
+        {
+            Decor();
+            Console.WriteLine("SEJA BEM VINDO AO RPG DA BLUE:");
+            Console.WriteLine("..... Escolha um nome para o seu jogador: ....");
+            Console.ReadLine();
+            Decor();
+        }
+
+        public void Decor()
+        {
+            string str = string.Concat(Enumerable.Repeat("-=-", 15));
+            Console.WriteLine(str);
+        }
+    }
+}
--- C#_projects/RPG_aula4/Program.cs
+++ C#_projects/RPG_aula4/Program.cs
@@ -6,7 +6,8 @@ namespace RPG_aula4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Play play = new Play();
+            play.Start();
         }
     }
 }
