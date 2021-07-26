fd4efe2e6ba515e8dac2777abc3633b52a78162d
Gabriel Santana <gabriellima36716@gmail.com>
2021-07-26 19:03:24 -0300

    exercícios da aula 8

---
+++ C#_projects/Consultorio/Controllers/MonstrosController.cs
@@ -0,0 +1,23 @@
+﻿using Consultorio.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Consultorio.Controllers
+{
+    public class MonstrosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Monstros> listaDeMonstros = new List<Monstros>();
+            listaDeMonstros.Add(new Monstros { id = 1, nome = "Zombie", ataque = 2});
+            listaDeMonstros.Add(new Monstros { id = 2, nome = "Esqueleto", ataque = 4});
+            listaDeMonstros.Add(new Monstros { id = 1, nome = "Creeper", ataque = 7});
+
+            ViewBag.monstros = listaDeMonstros;
+            return View();
+        }
+    }
+}
---
+++ C#_projects/Consultorio/Controllers/PacienteController.cs
@@ -0,0 +1,24 @@
+﻿using Consultorio.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Consultorio.Controllers
+{
+    public class PacienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Paciente> listaDePacientes = new List<Paciente>();
+            listaDePacientes.Add(new Paciente {  id = 1, nome = "Gabriel Lima"});
+            listaDePacientes.Add(new Paciente {  id = 2, nome = "Karolaine Santana"});
+            listaDePacientes.Add(new Paciente {  id = 3, nome = "Rafael Almeida"});
+            listaDePacientes.Add(new Paciente {  id = 4, nome = "Matheus dos Santos"});
+
+            ViewBag.pacientes = listaDePacientes;
+            return View();
+        }
+    }
+}
