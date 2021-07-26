35894378f4f6dc670b2daeda93931bd846120e63
Gabriel Santana <gabriellima36716@gmail.com>
2021-07-26 15:30:56 -0300

    parte 3 do codelab da aula 7

--- C#_projects/WebApplication1/Models/Aluno.cs
+++ C#_projects/WebApplication1/Models/Aluno.cs
@@ -7,5 +7,16 @@ namespace WebApplication1.Models
 {
     public class Aluno
     {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public DateTime data_nasc { get; set; }
+        public int idade { get => Convert.ToInt32((DateTime.Now - data_nasc).TotalDays/365); }
+
+        public Aluno(int id, string nome, DateTime data_nasc)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.data_nasc = data_nasc;
+        }
     }
 }
--- C#_projects/WebApplication1/Pages/Alunos/Index.cshtml.cs
+++ C#_projects/WebApplication1/Pages/Alunos/Index.cshtml.cs
@@ -4,13 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1.Models;
 
 namespace WebApplication1.Pages.Alunos
 {
     public class IndexModel : PageModel
     {
+        public List<Aluno> alunos = new();
         public void OnGet()
         {
+            this.alunos.Add(new Aluno(1, "Gabriel", new DateTime(1997,08,07)));
+            this.alunos.Add(new Aluno(2, "Karol", new DateTime(1997,04,08)));
+            this.alunos.Add(new Aluno(3, "Amana", new DateTime(2000,11,21)));
+            this.alunos.Add(new Aluno(4, "Jemima", new DateTime(1992,11,04)));
         }
     }
 }
